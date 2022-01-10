212f001597ca5f5e59c733eb80baad367d429dc8
aaa <aaa@gmail.com>
2022-01-10 19:45:11 +0530

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-pwa1';
+export class AppComponent implements OnInit{
+ isOnline:boolean;
+ constructor()
+ {
+   this.isOnline=false;
+ }
+  ngOnInit(): void {
+   // throw new Error('Method not implemented.');
+ this.updateOnlineStatus
+
+  window.addEventListener('online',this.updateOnlineStatus.bind(this));
+  window.addEventListener('offline',this.updateOnlineStatus.bind(this));
+  }
+
+  private updateOnlineStatus():void{
+this.isOnline=window.navigator.onLine;
+console.info('isOnline=[${this.isOnline}]');
+   
+  }
+
 }
