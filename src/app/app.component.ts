02d427688d0996f1e5f40a8bf33ef7faceaad0a9
aaa <aaa@gmail.com>
2022-01-10 21:12:41 +0530

    pwa2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,84 @@
 import { Component, OnInit } from '@angular/core';
-import { SwUpdate,VersionReadyEvent } from '@angular/service-worker';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { filter, map } from 'rxjs';
+import { Platform } from '@angular/cdk/platform'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
- isOnline:boolean;
- modalVersion: boolean ;
- constructor(private swUpdate:SwUpdate)
- {
-   this.isOnline=false;
-   this.modalVersion=false;
- }
+export class AppComponent implements OnInit {
+  isOnline: boolean;
+  modalVersion: boolean;
+  constructor(private swUpdate: SwUpdate,private platform:Platform) {
+    this.isOnline = false;
+    this.modalVersion = false;
+  }
   ngOnInit(): void {
-   // throw new Error('Method not implemented.');
- this.updateOnlineStatus();
+    // throw new Error('Method not implemented.');
+    this.updateOnlineStatus();
 
-  window.addEventListener('online',this.updateOnlineStatus.bind(this));
-  window.addEventListener('offline',this.updateOnlineStatus.bind(this));
- 
- if(this.swUpdate.isEnabled)
+    window.addEventListener('online', this.updateOnlineStatus.bind(this));
+    window.addEventListener('offline', this.updateOnlineStatus.bind(this));
+
+    if (this.swUpdate.isEnabled) {
+
+      this.swUpdate.versionUpdates.pipe(filter((evt: any): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+        map((evt: any) => {
+          console.log('currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]');
+          this.modalVersion = true;
+        }),
+      );
+
+    }
+this.loadModalPwa();
+  }
+
+
+
+
+
+
+
+
+  private updateOnlineStatus(): void {
+    this.isOnline = window.navigator.onLine;
+    console.info('isOnline=[${this.isOnline}]');
+
+  }
+
+  private loadModalPwa():void{
+ if(this.platform.ANDROID)
+
  {
-
-this.swUpdate.versionUpdates.pipe(filter((evt:any):evt is VersionReadyEvent=>evt.type==='VERSION_READY'),
-map((evt:any)=> {
-  console.log('currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]');
-  this.modalVersion=true;
-}),
-);
-
+//code will be platform specific
+ }
+ if(this.platform.isBrowser)
+ {
+//code will be platform 
  }
 
-  }
-
- 
-
-
-
-
-
-
-  private updateOnlineStatus():void{
-this.isOnline=window.navigator.onLine;
-console.info('isOnline=[${this.isOnline}]');
-   
-  }
-
- updateVersion():void{
-
-this.modalVersion=false;
-window.location.reload();
+if(this.platform.IOS)
+{
+//code will be platform specific
 
 }
 
-public closeVersion():void{
-this.modalVersion=false;
 
-}
+  }
+
+  updateVersion(): void {
+
+    this.modalVersion = false;
+    window.location.reload();
+
+  }
+
+  public closeVersion(): void {
+    this.modalVersion = false;
+
+  }
 
 }
 function evt(evt: any, any: any): import("rxjs").OperatorFunction<import("@angular/service-worker").VersionEvent, import("@angular/service-worker").VersionEvent> {
