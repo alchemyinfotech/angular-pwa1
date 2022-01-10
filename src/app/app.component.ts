e8357283130651f4a1ef081aafa4cf7bd13abc40
aaa <aaa@gmail.com>
2022-01-10 20:47:27 +0530

    pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SwUpdate,VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
  isOnline:boolean;
- constructor()
+ modalVersion: boolean ;
+ constructor(private swUpdate:SwUpdate)
  {
    this.isOnline=false;
+   this.modalVersion=false;
  }
   ngOnInit(): void {
    // throw new Error('Method not implemented.');
- this.updateOnlineStatus
+ this.updateOnlineStatus();
 
   window.addEventListener('online',this.updateOnlineStatus.bind(this));
   window.addEventListener('offline',this.updateOnlineStatus.bind(this));
+ 
+ if(this.swUpdate.isEnabled)
+ {
+
+this.swUpdate.versionUpdates.pipe(filter((evt:any):evt is VersionReadyEvent=>evt.type==='VERSION_READY'),
+map((evt:any)=> {
+  console.log('currentVersion=[${evt.currentVersion} | latestVersion=[${evt.latestVersion}]');
+  this.modalVersion=true;
+}),
+);
+
+ }
+
   }
+
+ 
+
+
+
+
+
 
   private updateOnlineStatus():void{
 this.isOnline=window.navigator.onLine;
@@ -25,4 +49,20 @@ console.info('isOnline=[${this.isOnline}]');
    
   }
 
+ updateVersion():void{
+
+this.modalVersion=false;
+window.location.reload();
+
 }
+
+public closeVersion():void{
+this.modalVersion=false;
+
+}
+
+}
+function evt(evt: any, any: any): import("rxjs").OperatorFunction<import("@angular/service-worker").VersionEvent, import("@angular/service-worker").VersionEvent> {
+  throw new Error('Function not implemented.');
+}
+
